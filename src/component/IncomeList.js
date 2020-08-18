e200235bb14169bce0515f74bdcc43eeff319baf
habeelwali <habeelwal3@gmail.com>
2020-08-18 21:35:52 +0000

    Update IncomeList.js

--- src/component/IncomeList.js
+++ src/component/IncomeList.js
@@ -4,7 +4,7 @@ import IncomeTransaction from './IncomeTransactions'
 
 const IncomeList = () => {
     const { incomeTransactions } = useContext(GlobalContext)
-    console.log(incomeTransactions);
+    
     return (
         <div className="transactions transactions-income">
             <h2>Transaction History</h2>
